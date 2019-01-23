bc6732f6da7603ce4b5eae6225b4d89573e033b1
mk-5 <git@mk5.pl>
2019-01-23 20:33:25 +0100

    Analytics API: Run setScreen on the main thread

--- gdx-fireapp-ios-moe/tests/mk/gdx/firebase/ios/analytics/AnalyticsTest.java
+++ gdx-fireapp-ios-moe/tests/mk/gdx/firebase/ios/analytics/AnalyticsTest.java
@@ -19,19 +19,23 @@ package mk.gdx.firebase.ios.analytics;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.mockito.internal.verification.VerificationModeFactory;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.moe.natj.general.NatJ;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 
 import java.util.Collections;
 
+import apple.NSObject;
+import apple.c.Globals;
 import apple.foundation.NSDictionary;
 import apple.foundation.NSMutableDictionary;
 import apple.foundation.NSString;
 import bindings.google.firebaseanalytics.FIRAnalytics;
 import mk.gdx.firebase.ios.GdxIOSAppTest;
 
-@PrepareForTest({FIRAnalytics.class, NatJ.class, NSDictionary.class, NSMutableDictionary.class, NSString.class})
+@PrepareForTest({FIRAnalytics.class, NatJ.class, NSDictionary.class, NSMutableDictionary.class, NSString.class, Globals.class})
 public class AnalyticsTest extends GdxIOSAppTest {
 
     @Override
@@ -62,8 +66,17 @@ public class AnalyticsTest extends GdxIOSAppTest {
     }
 
     @Test
-    public void setScreen() {
+    public void setScreen() throws Exception {
         // Given
+        PowerMockito.mockStatic(Globals.class);
+        PowerMockito.when(Globals.class, "dispatch_async", Mockito.nullable(NSObject.class), Mockito.any(Globals.Block_dispatch_async.class))
+                .then(new Answer() {
+                    @Override
+                    public Object answer(InvocationOnMock invocation) throws Throwable {
+                        ((Globals.Block_dispatch_async) invocation.getArgument(1)).call_dispatch_async();
+                        return null;
+                    }
+                });
         Analytics analytics = new Analytics();
 
         // When
